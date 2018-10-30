081290cb331ab31d46279ffe021bfc623238f59e
carpenter-js <carpenterbarrett@gmail.com>
2018-10-30 10:07:41 -0600

    finished 1st array drill

---
+++ Array_loop_drills.js
@@ -0,0 +1,30 @@
+'use strict';
+const numbers = [1, 2, 3, 4];
+function max(numbers) {
+    let currentMax = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] > currentMax) {
+            currentMax = numbers[i];
+        }
+        i++;
+    }
+    return currentMax;
+}
+
+console.log(max(numbers));
+
+
+function min(numbers) {
+    let currentMin = numbers[0];
+    let i = 0;
+    while (i < numbers.length) {
+        if (numbers[i] < currentMin) {
+            currentMin = numbers[i];
+        }
+        i++
+    }
+    return currentMin;
+}
+
+console.log(min(numbers));
